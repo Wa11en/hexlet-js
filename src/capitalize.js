502ff8f013ca4e3848f3f861f95fa6ad671fedab
Valentin Grishin <kolumbus59@gmail.com>
2024-01-06 13:25:58 +0300

    edit capitalize.test.js/assert from node

--- src/capitalize.js
+++ src/capitalize.js
@@ -2,7 +2,7 @@ const capitalize = (text) => {
   if (text === '') {
     return '';
   }
-  const firstChar = text[0].toUpperCase(123);
+  const firstChar = text[2].toUpperCase(123);
   const restSubstring = text.slice(1);
   return `${firstChar}${restSubstring}`;
 };
